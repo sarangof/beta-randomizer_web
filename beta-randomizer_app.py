ce11fe76e4eb3825123b7dc6991f0d2b158a42e7
Camilo Londoño <camiloll@outlook.com>
2019-11-15 19:44:45 -0500

    Add breadcrumb and new styles

--- beta-randomizer_app.py
+++ beta-randomizer_app.py
@@ -15,9 +15,10 @@ data = pd.DataFrame([])
 
 HTML_HEAD = '<title>Beta-Randomizer</title> \
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> \
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> \
+<link rel="stylesheet" href="/css/style.css"> \
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> \
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> \
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> '
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity = "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> '
 
 UPLOAD_FOLDER = '.'
 ALLOWED_EXTENSIONS = set(['xlsx','xlx'])
@@ -86,7 +87,7 @@ def randomization_options():
             #print(session['data'])
             data = standardize_columns(pd.DataFrame(ast.literal_eval(session['data'])))
             session['data'] = data.to_json()
-            return render_template('new-scheme-options.html', columns=data.columns)
+            return render_template('new-scheme-options.html', columns=data.columns, head=HTML_HEAD)
         else:
             return '''What should I do here?'''
 
@@ -193,6 +194,9 @@ def df_download(filename):
     return send_file(app.config['UPLOAD_FOLDER']+"/data_rand.xlsx", as_attachment=True)
     #return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
+@app.route('/css/<filename>')
+def serve_static(filename):
+    return send_file('static/css/'+filename)
 
 if __name__ == '__main__':
     app.debug = True
