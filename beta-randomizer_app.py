74332bf6a068ad64ad89e2f53b88f57e5994b135
Camilo Londoño <camiloll@outlook.com>
2020-02-06 10:42:22 -0500

    Change session to serve-side session

--- beta-randomizer_app.py
+++ beta-randomizer_app.py
@@ -3,13 +3,16 @@ from flask import Flask, flash, request, redirect, url_for, send_from_directory,
 from werkzeug.utils import secure_filename
 from betaRand_app_functions import stratify, standardize_columns, check_strat_file, update_stratification, create_plots
 import pandas as pd
-import ast
+import json
 
 import base64
 import matplotlib.pyplot as plt
-#global data 
+# global data
 
 from dateutil import parser
+
+from flask_session import Session
+
 
 data = pd.DataFrame([])
 
@@ -21,28 +24,33 @@ HTML_HEAD = '<title>Beta-Randomizer</title> \
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity = "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> '
 
 UPLOAD_FOLDER = '.'
-ALLOWED_EXTENSIONS = set(['xlsx','xlx'])
+ALLOWED_EXTENSIONS = set(['xlsx', 'xlx'])
 
 app = Flask(__name__)
-app.secret_key = "password"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config.from_object('config.Config')
+
+sess = Session()
+sess.init_app(app)
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/', methods=['GET', 'POST'])
 def choose_rand_option():
     session.clear()
     session['data'] = pd.DataFrame([]).to_json()
-    if request.method == 'POST':# NEED TO TURN THESE INTO POST REQUESTS INSTEAD OF REDIRECTS.
+    # NEED TO TURN THESE INTO POST REQUESTS INSTEAD OF REDIRECTS.
+    if request.method == 'POST':
         if request.form['desired_action'] == 'create':
             return redirect(url_for('create_scheme'))
         if request.form['desired_action'] == 'update':
             return redirect(url_for('update_scheme'))
     if request.method == 'GET':
         return render_template('welcome.html', head=HTML_HEAD)
+
 
 @app.route('/create_scheme', methods=['GET', 'POST'])
 def create_scheme():
@@ -60,44 +68,47 @@ def create_scheme():
         if file.filename == '':
             flash('No selected file')
             print('No selected file')
-            return redirect(request.url)#redirect(request.url)
+            return redirect(request.url)  # redirect(request.url)
 
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file_all = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_all)
-            
+
             session['data'] = pd.read_excel(file_all).to_json()
-            print("session['data']")
-            print(session['data'])
-            return redirect(url_for('randomization_options'), code=307)#redirect(url_for('randomization_options'))
+            # print("session['data']")
+            # print(session['data'])
+            # redirect(url_for('randomization_options'))
+            return redirect(url_for('randomization_options'), code=307)
 
     else:
-        #session['data'] = None
+        # session['data'] = None
         # redirect(request.url)#render_template('create_scheme.html',code=302)
         return render_template('create_scheme.html', head=HTML_HEAD)
 
 
-@app.route('/randomization_options',methods=['GET','POST'])
+@app.route('/randomization_options', methods=['GET', 'POST'])
 def randomization_options():
     if session.get('data') is not None:
-        print("session['data']")
-        print(session['data'])
-        data = standardize_columns(pd.DataFrame(ast.literal_eval(session['data'])))
+        # print("session['data']")
+        # print(session['data'])
+        data = standardize_columns(
+            pd.DataFrame(json.loads(session['data'])))
         session['data'] = data.to_json()
         return render_template('new-scheme-options.html', columns=data.columns, head=HTML_HEAD)
     else:
         # Return to previous step
         return redirect(url_for('create_scheme'))
 
-@app.route('/update_scheme', methods=['GET','POST'])
+
+@app.route('/update_scheme', methods=['GET', 'POST'])
 def update_scheme():
     if request.method == 'POST':
         print("postUPDATESCHEME")
         session['update'] = True
         if (('file_new' not in request.files) or ('file_RCT' not in request.files)):
             flash('No file part')
-            #print('No file part')
+            # print('No file part')
             return redirect(request.url)
         file_new = request.files['file_new']
         file_RCT = request.files['file_RCT']
@@ -108,61 +119,68 @@ def update_scheme():
             return redirect(request.url)
 
         if file_new and file_RCT and allowed_file(file_new.filename) and allowed_file(file_RCT.filename):
-  
+
             print("allowed files for scheme updation")
             filename_RCT = secure_filename(file_RCT.filename)
             filename_new = secure_filename(file_new.filename)
 
-            file_all_RCT = os.path.join(app.config['UPLOAD_FOLDER'], filename_RCT)
-            file_all_new = os.path.join(app.config['UPLOAD_FOLDER'], filename_new)
+            file_all_RCT = os.path.join(
+                app.config['UPLOAD_FOLDER'], filename_RCT)
+            file_all_new = os.path.join(
+                app.config['UPLOAD_FOLDER'], filename_new)
 
             file_RCT.save(file_all_RCT)
             file_new.save(file_all_new)
-            
-            session['data_rct'] = pd.read_excel(file_all_RCT).to_json(date_format='iso', date_unit='s')
-            #print("session['data_rct'] JSON VERSION")
-            #print(session['data_rct'])
+
+            session['data_rct'] = pd.read_excel(file_all_RCT).to_json(
+                date_format='iso', date_unit='s')
+            # print("session['data_rct'] JSON VERSION")
+            # print(session['data_rct'])
             session['data_new'] = pd.read_excel(file_all_new).to_json()
             session['filename1'] = str(file_RCT.filename)
 
-            return redirect(url_for('visualize_scheme'),code=307)
+            return redirect(url_for('visualize_scheme'), code=307)
 
     else:
         print("GETUPDATESCHEME")
-        #session['data'] = False
+        # session['data'] = False
         session['update'] = False
 
         return render_template('update_scheme.html', head=HTML_HEAD)
 
-@app.route('/visualize_scheme', methods=['GET','POST'])
+
+@app.route('/visualize_scheme', methods=['GET', 'POST'])
 def visualize_scheme():
     if request.method == 'POST':
-        #print("session['update']")
-        #print(session['update'])
+        # print("session['update']")
+        # print(session['update'])
         data_rand = pd.DataFrame([])
         strat_columns = []
         pure_randomization_boolean = False
         sample_p = 50.
         filename = 'data_rand.xlsx'
-        if session['update']==True:
-            session_update=True
-            data_rct = pd.DataFrame(ast.literal_eval(session['data_rct']))
-            data_new = pd.DataFrame(ast.literal_eval(session['data_new']))
+        if session['update'] == True:
+            session_update = True
+            data_rct = pd.DataFrame(json.loads(session['data_rct']))
+            data_new = pd.DataFrame(json.loads(session['data_new']))
             filename1 = session['filename1']
-            valid_update, message_update, pure_randomization_boolean, strat_columns, sample_p = check_strat_file(data_rct, data_new, session['filename1'])
+            valid_update, message_update, pure_randomization_boolean, strat_columns, sample_p = check_strat_file(
+                data_rct, data_new, session['filename1'])
             if valid_update:
-                data_rand, strat_columns, filename = update_stratification(data_rct, data_new, session['filename1'], pure_randomization_boolean, strat_columns)
+                data_rand, strat_columns, filename = update_stratification(
+                    data_rct, data_new, session['filename1'], pure_randomization_boolean, strat_columns)
                 session['data_rand'] = data_rand.to_json()
             else:
                 flash(message_update)
-                #redirect(url_for('update_scheme'))
+                # redirect(url_for('update_scheme'))
         else:
-            session_update=False
+            session_update = False
             # THERE WILL BE AN ERROR HERE. GET with update vs get without an update.
             print("create statement")
-            data_set = pd.DataFrame(ast.literal_eval(session['data']))
+            data_set = pd.DataFrame(json.loads(session['data']))
             strat_columns = []
-            sample_p = int(request.form.get('sample_p'))#int(request.form['sample_p'])
+            # int(request.form['sample_p'])
+            sample_p = int(request.form.get('sample_p'))
             for cols in data_set.columns:
                 if request.form.get(cols) == '1':
                     strat_columns.append(str(cols))
@@ -170,35 +188,41 @@ def visualize_scheme():
             rand_type = request.form.get('randomization_type')
             if rand_type == 'Simple':
                 data_rand, filename = stratify(data_set, strat_columns,
-                                        pure_randomization_boolean=True, 
-                                        sample_p=sample_p)
+                                               pure_randomization_boolean=True,
+                                               sample_p=sample_p)
 
             elif rand_type == 'Stratified':
                 data_rand, filename = stratify(data_set, strat_columns,
-                                    pure_randomization_boolean=False, 
-                                    sample_p=sample_p)
+                                               pure_randomization_boolean=False,
+                                               sample_p=sample_p)
 
         if (not data_rand.empty):
-            viz_list = create_plots(data_rand,strat_columns,pure_randomization_boolean,sample_p,session_update)
-            
-            #return render_template('visualize_scheme.html', data_rand = data_rand, plot_url = plot_url)#send_file(app.config['UPLOAD_FOLDER']+"/the-global-city-brown.pdf", as_attachment=True)
+            viz_list = create_plots(
+                data_rand, strat_columns, pure_randomization_boolean, sample_p, session_update)
+
+            # return render_template('visualize_scheme.html', data_rand = data_rand, plot_url = plot_url)#send_file(app.config['UPLOAD_FOLDER']+"/the-global-city-brown.pdf", as_attachment=True)
             return render_template('visualize_scheme.html', viz_list=viz_list, head=HTML_HEAD, filename=filename, referer=request.headers.get("Referer"))
 
-                    #send_from_directory(app.config['UPLOAD_FOLDER'], "the-global-city-brown.pdf", as_attachment=True), \
+            # send_from_directory(app.config['UPLOAD_FOLDER'], "the-global-city-brown.pdf", as_attachment=True), \
+        else:
+            return render_template('visualize_scheme error.html', head=HTML_HEAD, referer=request.headers.get("Referer"))
     else:
         # Return to home
         return redirect(url_for('choose_rand_option'))
-    
-@app.route('/df_download/<filename>',methods=['GET','POST'])
+
+
+@app.route('/df_download/<filename>', methods=['GET', 'POST'])
 def df_download(filename):
-    return send_file("{}/{}".format(app.config['UPLOAD_FOLDER'],filename), as_attachment=True)
-    #return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+    return send_file("{}/{}".format(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
+    # return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+
 
 @app.route('/css/<filename>')
 def serve_static(filename):
     return send_file('static/css/'+filename)
 
+
 if __name__ == '__main__':
     app.debug = True
-    #app.run(host='0.0.0.0', port=4500)
+    # app.run(host='0.0.0.0', port=4500)
     app.run()
---
+++ config.py
@@ -0,0 +1,18 @@
+"""App configuration."""
+from os import environ
+import redis
+
+
+class Config:
+    """Set Flask configuration vars from .env file."""
+
+    # General Config
+    SECRET_KEY = environ.get('SECRET_KEY')
+    FLASK_APP = environ.get('FLASK_APP')
+    FLASK_ENV = environ.get('FLASK_ENV')
+    UPLOAD_FOLDER = '.'
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+    # Flask-Session
+    SESSION_TYPE = environ.get('SESSION_TYPE')
+    SESSION_REDIS = redis.from_url(environ.get('SESSION_REDIS'))
